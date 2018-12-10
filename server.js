6ca69fe20782cfdba06f5e72b952793543a4ae87
haleybarlar <hbarlar1@gmail.com>
2018-12-10 11:29:39 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+// nodemon server.js -e js, hbs
+
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const port = process.env.PORT || 3000
+
+let app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+// middleware
+// creates server.log
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+// Root Route:
+app.get('/', (request, response) => {
+  // response.send('<h1>hello, world!</h1>')
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: "Yo welcome to da site"
+  })
+})
+
+// another route:
+app.get('/about', (req, resp) => {
+  resp.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+})
+
+// create route to /bad
+// send back JSON w errorMessage ('unable to handle request')
+app.get('/bad', (req, resp) => {
+  resp.send({
+    errorMessage: 'unable to handle request'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
+})
